f33315c8c168a8e3347e2c02db4512f8a83014a2
Steven Pouzon <spouzon@gmail.com>
2021-05-10 12:37:16 +0200

    stupid coach rails app ok

---
+++ test/system/questions_test.rb
@@ -0,0 +1,26 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "p", text: "Ask your coach anything"
+  end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test "saying Iam going to work yields the great answer" do
+    visit ask_url
+    fill_in "question", with: "I am going to work"
+    take_screenshot
+    click_on "Ask"
+
+    assert_text "Great!"
+  end
+
+end
